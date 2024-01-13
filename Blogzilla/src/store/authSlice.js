421d6a4034b5ce4d8b7a66b959f96507839feb03
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-01-13 20:28:25 +0530

    making store (reducers) for blogzilla

---
+++ Blogzilla/src/store/authSlice.js
@@ -0,0 +1,30 @@
+// authSlice (Slice) is good practice for naming contevention if we are using  create VITE project using redux/toolkit {same as create react project}
+
+// this store tracks user authentications if user is authenticated or not
+
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+     }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
