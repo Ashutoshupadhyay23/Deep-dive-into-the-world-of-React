40c101fa177772ee70b9279857a3283dbf9f853d
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-07-01 17:16:14 +0530

    minor changes

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+// import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
+import { Header, Footer } from './components'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <>
-        <Header />
-        <Outlet />
-        <Footer />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 
 function Github() {
 
@@ -21,10 +21,30 @@ function Github() {
   
 
   return (
-    <div className='bg-gray-500 text-center m-4 text-white text-3xl p-4'>
-        Github followers: {data.followers}
-        <img src={data.avatar_url} alt="Git picture" width={250} />
+    <div className='bg-gray-500 text-white text-3xl p-4 relative'>
+  
+      <div className="absolute top-0 left-0 p-4 text-2xl">
+        Username: {" "}
+        <Link 
+          to={data.html_url} 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-blue-500 transition-transform duration-300 ease-in-out transform hover:-translate-y-1"
+        >
+          {data.name}
+        </Link>
+      </div>
+      
+      <div className="absolute top-0 right-0 p-4">
+        <img src={data.avatar_url} alt="Git picture" width={50} className="rounded-full" />
+      </div>
+
+      <div className="text-center mt-20">
+        <div className="text-3xl">Public Repos: {data.public_repos}</div>
+      </div>
     </div>
+
+
   )
 }
 
